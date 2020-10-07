7d56140347304a0605ed706a66f863526eea9ffe
Michael Murad <neverthegreat@icloud.com>
2020-10-06 22:41:24 -0700

    Updated tests to be more specific

--- src/__tests__/header.test.tsx
+++ src/__tests__/header.test.tsx
@@ -2,8 +2,14 @@ import React from "react"
 import { render } from "@testing-library/react"
 import Header from "../components/header"
 
-it("renders correctly", () => {
-  const { container } = render(<Header siteTitle="hello" />)
-  expect(container.firstChild.textContent).toBe("hello")
-  expect(container.querySelector("header").className).toMatchSnapshot()
+describe("Header component", () => {
+  it("renders correctly", () => {
+    const { container } = render(<Header siteTitle="hello" />)
+    expect(container.firstChild.textContent).toBe("hello")
+  })
+
+  it("has unchanged css", () => {
+    const { container } = render(<Header siteTitle="hello" />)
+    expect(container.querySelector("header").className).toMatchSnapshot()
+  })
 })
