42327b32aa735c1adcf82f9097e80151efca2c94
Michael Murad <neverthegreat@icloud.com>
2020-10-06 22:37:24 -0700

    Updated test to avoid TS error

--- src/__tests__/header.test.tsx
+++ src/__tests__/header.test.tsx
@@ -5,6 +5,5 @@ import Header from "../components/header"
 it("renders correctly", () => {
   const { container } = render(<Header siteTitle="hello" />)
   expect(container.firstChild.textContent).toBe("hello")
-  // @ts-ignore: Property 'classList' does not exist on type 'ChildNode'
-  expect(container.firstChild.classList).toMatchSnapshot()
+  expect(container.querySelector("header").className).toMatchSnapshot()
 })
